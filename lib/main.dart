4b0600d77617c30cabbb89dc46744f9ee98d786a
ahmedrtrimohamed-ui <ahmedrtrimohamed-ui@users.noreply.github.com>
2026-07-30 19:26:16 +0300

    Fix SharedPreferences init in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'core/theme/dark_theme.dart';
 import 'features/splash/splash_screen.dart';
+import 'data/providers/storage_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Set preferred orientations
+  final prefs = await SharedPreferences.getInstance();
+
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
 
-  // Set status bar color
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
@@ -21,7 +23,14 @@ void main() async {
     ),
   );
 
-  runApp(const ProviderScope(child: TouGameApp()));
+  runApp(
+    ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(prefs),
+      ],
+      child: const TouGameApp(),
+    ),
+  );
 }
 
 class TouGameApp extends StatelessWidget {
